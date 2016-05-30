a48f7709106bc316ee94c1e0f4f90c7a03066863
kojirou <kojirouhtc@gmail.com>
2016-05-31 02:06:45 +0800

    Initial Commit

---
+++ KVOTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KVOTest
+//
+//  Created by 王宇 on 16/5/31.
+//  Copyright © 2016年 Kojirou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
